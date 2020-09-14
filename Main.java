d427debe21d0beaac0daa45c35fce3f8153837c8
blisn8405 <blisn8405@wrdsb.ca>
2020-09-14 15:07:29 +0000

    finished task 1

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+// create a Scanner for user input
+ Scanner input = new Scanner(System.in);
+
+ // declare a variable to store Celsius
+
+ // ask the user for celsius
+  System.out.println("Please enter the Celsius that you want to change to Fahrenheit:");
+  // initialize the celsius that needs to be converted to Fahrenheit
+
+
+// declare and initialize a variable for Celsius
+int Celsius = input.nextInt();
+
+// delcare and initialize a variable for Fahrenheit
+int Fahrenheit = ((Celsius*9)/5 +32);
+ // tell the user the converted temperature
+ System.out.println("The answer is "  + Fahrenheit + "F.");
   }
 }
